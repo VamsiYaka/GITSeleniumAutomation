9bd879a1ca1de40edfb30647995a25c7a92a7440
VamsiYaka <vamsiyaka@gmail.com>
2025-01-25 00:31:34 +0530

    nextComit

--- src/Menu.java
+++ src/Menu.java
@@ -3,6 +3,11 @@ public class Menu {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
+		
 
 	}
 
